5a7cff8591ff91380002987d34575611568721ef
Fielnando <114557650+Fielnando@users.noreply.github.com>
2022-09-28 23:57:32 -0500

    Add files via upload

---
+++ P27S2120222HRLF/app/src/main/java/mx/edu/tesoem/isc/g7s21/p27s2120222jdga/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.g7s21.p27s2120222jdga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
